2737e6d8f5a3c91f53b477d4b6b0ee78020996e9
MohonSaha <practicewithmohon@gmail.com>
2023-04-07 16:32:52 +0600

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+const Home = () => {
+
+    const tShirts = useLoaderData();
+    console.log(tShirts);
+
+    return (
+        <div>
+            <h2>This is home.</h2>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h2>I am order review</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
